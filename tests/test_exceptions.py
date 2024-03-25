84fb36720452882b9d6fe95dfe27f798819606d2
Pavel Dedik <dedikx@gmail.com>
2024-03-25 11:59:25 +0100

    test: Cover exceptions

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -1,8 +1,24 @@
+import pytest
 from starlette.testclient import TestClient
 
 from ludic.html import p
 from ludic.web import LudicApp, Request
-from ludic.web.exceptions import NotFoundError
+from ludic.web.exceptions import (
+    BadGatewayError,
+    BadRequestError,
+    ClientError,
+    ForbiddenError,
+    GatewayTimeoutError,
+    InternalServerError,
+    MethodNotAllowedError,
+    NotFoundError,
+    NotImplementedError,
+    PaymentRequiredError,
+    ServerError,
+    ServiceUnavailableError,
+    TooManyRequestsError,
+    UnauthorizedError,
+)
 
 app = LudicApp()
 
@@ -42,7 +58,7 @@ async def server_error(exception: Exception) -> p:
     return p(f"server error: {exception}")
 
 
-def test_exceptions() -> None:
+def test_exception_handling() -> None:
     test_data = [
         ("/", 200, "hello world"),
         ("/not-found", 404, "page /not-found does not exist"),
@@ -55,3 +71,32 @@ def test_exceptions() -> None:
             response = client.get(path)
             assert response.status_code == status
             assert response.text == p(content).to_html()
+
+
+def test_exceptions() -> None:
+    test_data = [
+        (ClientError, 400),
+        (BadRequestError, 400),
+        (UnauthorizedError, 401),
+        (PaymentRequiredError, 402),
+        (ForbiddenError, 403),
+        (NotFoundError, 404),
+        (MethodNotAllowedError, 405),
+        (TooManyRequestsError, 429),
+        (ServerError, 500),
+        (InternalServerError, 500),
+        (NotImplementedError, 501),
+        (BadGatewayError, 502),
+        (ServiceUnavailableError, 503),
+        (GatewayTimeoutError, 504),
+    ]
+
+    for Error, status_code in test_data:
+        assert issubclass(Error, Exception)
+        with pytest.raises(Error) as error:
+            raise Error("test message")
+
+        assert (
+            error.exconly()
+            == f"ludic.web.exceptions.{Error.__name__}: {status_code}: test message"
+        )
