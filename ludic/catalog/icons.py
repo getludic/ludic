bcf7337f5c526384b2c2f192d0eacafe497e6e0d
Pavel Dedik <dedikx@gmail.com>
2024-05-26 19:13:54 +0200

    feat(catalog): Add icons support

---
+++ ludic/catalog/icons.py
@@ -0,0 +1,39 @@
+from typing import override
+
+from ludic.attrs import GlobalAttrs, ImgAttrs
+from ludic.base import AnyChildren
+from ludic.html import img, span, style
+from ludic.types import Component, ComponentStrict, NoChildren
+
+
+class Icon(Component[NoChildren, ImgAttrs]):
+    classes = ["icon"]
+    styles = {
+        ".icon": {
+            "width": "0.75em",
+            "height": "0.75em",
+        },
+    }
+
+    @override
+    def render(self) -> img:
+        return img(*self.children, **self.attrs)
+
+
+class WithIcon(ComponentStrict[Icon, AnyChildren, GlobalAttrs]):
+    classes = ["with-icon"]
+    styles = style.use(
+        lambda theme: {
+            ".with-icon": {
+                "display": "inline-flex",
+                "align-items": "baseline",
+            },
+            ".with-icon .icon": {
+                "margin-inline-end": theme.sizes.s,
+            },
+        }
+    )
+
+    @override
+    def render(self) -> span:
+        return span(*self.children, **self.attrs)
